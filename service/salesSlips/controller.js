342af8d5d1367b44f6dc2d52e454fef25521c7cc
yoonhae.oh <yoonhae.oh@greenlabs.co.kr>
2022-03-18 10:44:43 +0900

    공통DB의 관리화...

--- service/salesSlips/controller.js
+++ service/salesSlips/controller.js
@@ -2,10 +2,16 @@ const service = require('./service');
 const commonUtils = require('../../utils/commonUtils')
 
 const getSalesSlipKeys = async (req, res, next) => {
-    const { user_code, table_prefix, sa_iock, sa_code, sa_gcode, sa_area, searchValue, startDate, endDate, pageNum, limit, credit } = req.query;
-    const pageNation = commonUtils.getPageNation(limit, pageNum);
-    const result = await service.getSalesSlipKeys(user_code, table_prefix, startDate, endDate, pageNation.offset, pageNation.limit)
-    res.json(result);
+    try {
+        const { user_code, table_prefix, sa_iock, sa_code, sa_gcode, sa_area, searchValue, startDate, endDate, pageNum, limit, credit } = req.query;
+        const pageNation = commonUtils.getPageNation(limit, pageNum);
+        const result = await service.getSalesSlipKeys(user_code, table_prefix, startDate, endDate, pageNation.offset, pageNation.limit)
+        res.json(result);
+    } catch(error) {
+        // 동기 상태에서 exception 발생하는 경우에는 next 처리하지 않아도 알아서 error router 에서 처리가 되지만 
+        // 비동기 로직에서 exception 발생시 그대로 뻗어버려서 next 처리를 명시적으로 해야함.
+        next(error)
+    }
 }
 
 
--- utils/database.js
+++ utils/database.js
@@ -3,10 +3,33 @@ const SequelizeManager = require('./sequelize-manager');
 const config = require('config');
 const databaseConfig = config.get('databaseConfig');
 
+// 고객 DB 말고, 공통 DB의 sequelizeManager 객체를 별도로 관리하여 Model 객체의 캐싱을 고려한다.
+const sequelizeManagerDict = {}
 
-const getTablePrefix = async (user_code) => {
+async function getConnection (user_code, table_prefix) {
+  // 공통 DB 는 SequelizeManager 자체를 한번 더 관리함.
+  if(SequelizeManager.COMMON_DB_LIST.includes(user_code)) {
+    if (!(user_code in sequelizeManagerDict)) {
+      sequelizeManagerDict[user_code] = new SequelizeManager(databaseConfig, user_code);
+    }
+    
+    return sequelizeManagerDict[user_code]
+  } else {
+    // 일반 고객 DB는 매번 생성함.(모든 고객 DB의 Model 까지 캐시하면 메모리가....)
+    const conn = new SequelizeManager(databaseConfig, user_code, table_prefix);
+    
+    if (!table_prefix && !SequelizeManager.COMMON_DB_LIST.includes(this.user_code)) {
+        conn.table_prefix = await getTablePrefix(user_code);
+    }
+
+    return conn;
+  }
+}
+
+
+async function getTablePrefix(user_code) {
     if (!SequelizeManager.COMMON_DB_LIST.includes(user_code)) {
-      const conn = new SequelizeManager(databaseConfig, "woosung");
+      const conn = await getConnection("woosung");
       const { year } = await conn.models.ws_user_table.findOne({
         attributes: [[conn.sequelize.fn('max', Sequelize.col('year')), 'year']],
         where: {
@@ -26,14 +49,5 @@ const getTablePrefix = async (user_code) => {
     return null;
   }
 
-  const getConnection = async (user_code, table_prefix) => {
-    const conn = new SequelizeManager(databaseConfig, user_code, table_prefix);
-    
-    if (!table_prefix && !SequelizeManager.COMMON_DB_LIST.includes(this.user_code)) {
-        conn.table_prefix = await getTablePrefix(user_code);
-    }
-
-    return conn;
-}
 
 module.exports = {getConnection, getTablePrefix}
